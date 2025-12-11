fcc361e1eec115917ed671c7c4909378b03bf2dd
Ognjen Kremenović <ognen.kremenovic@vodafoneziggo.com>
2025-12-11 16:26:47 +0100

    prompts

--- exercises/04-persistence/04.04-persistence-in-a-normalized-db/explainer/main.ts
+++ exercises/04-persistence/04.04-persistence-in-a-normalized-db/explainer/main.ts
@@ -2,35 +2,35 @@ import { mapUIMessagePartsToDBParts } from './mapping.ts';
 import type { MyUIMessage } from './types.ts';
 
 const message: MyUIMessage = {
-  id: '123',
-  role: 'user',
-  parts: [
-    {
-      type: 'text',
-      text: 'Hello!',
-    },
-    {
-      type: 'reasoning',
-      text: 'I am thinking...',
-    },
-    {
-      type: 'tool-getWeatherInformation',
-      state: 'output-available',
-      toolCallId: '123',
-      input: {
-        city: 'London',
-      },
-      output: {
-        city: 'London',
-        weather: 'sunny',
-      },
-    },
-  ],
+    id: '123',
+    role: 'user',
+    parts: [
+        {
+            type: 'text',
+            text: 'Hello!',
+        },
+        {
+            type: 'reasoning',
+            text: 'I am thinking...',
+        },
+        {
+            type: 'tool-getWeatherInformation',
+            state: 'output-available',
+            toolCallId: '123',
+            input: {
+                city: 'London',
+            },
+            output: {
+                city: 'London',
+                weather: 'sunny',
+            },
+        },
+    ],
 };
 
 const dbMessageParts = mapUIMessagePartsToDBParts(
-  message.parts,
-  message.id,
+    message.parts,
+    message.id,
 );
 
 console.dir(dbMessageParts, { depth: null });
--- exercises/05-context-engineering/05.02-basic-prompting/problem/main.ts
+++ exercises/05-context-engineering/05.02-basic-prompting/problem/main.ts
@@ -6,16 +6,37 @@ const INPUT = `Do some research on induction hobs and how I can replace a 100cm 
 // NOTE: A good output would be: "Induction hobs vs AGA cookers"
 
 const result = await streamText({
-  model: google('gemini-2.0-flash-lite'),
-  // TODO: Rewrite this prompt using the Anthropic template from
-  // the previous exercise.
-  // You will NOT need all of the sections from the template.
-  prompt: `
-    Generate me a title:
-    ${INPUT}
+    model: google('gemini-2.0-flash-lite'),
+    // TODO: Rewrite this prompt using the Anthropic template from
+    // the previous exercise.
+    // You will NOT need all of the sections from the template.
+    prompt: `
+    <task-context>
+        You will be acting like an experienced blogger with a lot of SEO knowledge.
+        Your goal is to make a catchy title that will bring potential users to our site. Basically, we are doing here content marketing.
+    </task-context>
+    <tone-context>
+        Be creative, be funny, make a rhyme or make a pun. Just don't be rude.
+    </tone-context>
+    <examples>
+        You can find some ideas on this [This is an external link to databox.com](link https://databox.com/blog-title-examples).
+    </examples>
+    <the-ask>
+        Here is user input:
+        <question>
+            ${INPUT}
+        </question>
+        Your goal is to generate A SINGLE TITLE FOR TOPIC - ${INPUT}.
+    </the-ask>
+    <rules>
+        Do not write anything else except the title.
+    </rules>
+    <output-formatting>
+       **Title**
+    </output-formatting
   `,
 });
 
 for await (const chunk of result.textStream) {
-  process.stdout.write(chunk);
+    process.stdout.write(chunk);
 }
