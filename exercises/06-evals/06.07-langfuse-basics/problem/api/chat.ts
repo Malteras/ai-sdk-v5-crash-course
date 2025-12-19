32f59210ec87b4427b47584855d2e5e593278977
Ognjen Kremenovic <ognjen.kremenovic@codaxy.com>
2025-12-19 18:04:49 +0100

    test langfuse

--- exercises/06-evals/06.07-langfuse-basics/problem/api/chat.ts
+++ exercises/06-evals/06.07-langfuse-basics/problem/api/chat.ts
@@ -1,93 +1,105 @@
 import { google } from '@ai-sdk/google';
 import {
-  convertToModelMessages,
-  createUIMessageStreamResponse,
-  generateText,
-  streamText,
-  type ModelMessage,
-  type UIMessage,
+    convertToModelMessages,
+    createUIMessageStreamResponse,
+    generateText,
+    streamText,
+    type ModelMessage,
+    type UIMessage,
 } from 'ai';
 import { langfuse } from './langfuse.ts';
 
 export const POST = async (req: Request): Promise<Response> => {
-  const body = await req.json();
+    const body = await req.json();
 
-  const messages: UIMessage[] = body.messages;
+    const messages: UIMessage[] = body.messages;
 
-  const modelMessages: ModelMessage[] =
-    convertToModelMessages(messages);
+    const modelMessages: ModelMessage[] =
+        convertToModelMessages(messages);
 
-  // TODO: declare the trace variable using the langfuse.trace method,
-  // and pass it the following arguments:
-  // - sessionId: body.id
-  const trace = TODO;
+    // TODO: declare the trace variable using the langfuse.trace method,
+    // and pass it the following arguments:
+    // - sessionId: body.id
+    const trace = langfuse.trace({
+        sessionId: body.id,
+    });
 
-  const mostRecentMessage = messages[messages.length - 1];
+    const mostRecentMessage = messages[messages.length - 1];
 
-  if (!mostRecentMessage) {
-    return new Response('No messages provided', { status: 400 });
-  }
+    if (!mostRecentMessage) {
+        return new Response('No messages provided', {
+            status: 400,
+        });
+    }
 
-  const mostRecentMessageText = mostRecentMessage.parts
-    .map((part) => {
-      if (part.type === 'text') {
-        return part.text;
-      }
-      return '';
-    })
-    .join('');
+    const mostRecentMessageText = mostRecentMessage.parts
+        .map((part) => {
+            if (part.type === 'text') {
+                return part.text;
+            }
+            return '';
+        })
+        .join('');
 
-  const titleResult = generateText({
-    model: google('gemini-2.0-flash-lite'),
-    prompt: `
-      You are a helpful assistant that can generate titles for conversations.
+    const titleResult = generateText({
+        model: google('gemini-2.0-flash-lite'),
+        prompt: `
+            You are a helpful assistant that can generate titles for conversations.
 
-      <conversation-history>
-      ${mostRecentMessageText}
-      </conversation-history>
-      
-      Find the most concise title that captures the essence of the conversation.
-      Titles should be at most 30 characters.
-      Titles should be formatted in sentence case, with capital letters at the start of each word. Do not provide a period at the end.
-      Use no punctuation or emojis.
-      If there are acronyms used in the conversation, use them in the title.
-      Use formal language in the title, like 'troubleshooting', 'discussion', 'support', 'options', 'research', etc.
-      Since all items in the list are conversations, do not use the word 'chat', 'conversation' or 'discussion' in the title - it's implied by the UI.
-      The title will be used for organizing conversations in a chat application.
+            <conversation-history>
+            ${mostRecentMessageText}
+            </conversation-history>
 
-      Generate a title for the conversation.
-      Return only the title.
-    `,
-    // TODO: declare the experimental_telemetry property using the following object:
-    // - isEnabled: true
-    // - functionId: 'your-name-here'
-    // - metadata: { langfuseTraceId: trace.id }
-    experimental_telemetry: TODO,
-  });
+            Find the most concise title that captures the essence of the conversation.
+            Titles should be at most 30 characters.
+            Titles should be formatted in sentence case, with capital letters at the start of each word. Do not provide a period at the end.
+            Use no punctuation or emojis.
+            If there are acronyms used in the conversation, use them in the title.
+            Use formal language in the title, like 'troubleshooting', 'discussion', 'support', 'options', 'research', etc.
+            Since all items in the list are conversations, do not use the word 'chat', 'conversation' or 'discussion' in the title - it's implied by the UI.
+            The title will be used for organizing conversations in a chat application.
 
-  const streamTextResult = streamText({
-    model: google('gemini-2.0-flash'),
-    messages: modelMessages,
-    // TODO: declare the experimental_telemetry property using the following object:
-    // - isEnabled: true
-    // - functionId: 'your-name-here'
-    // - metadata: { langfuseTraceId: trace.id }
-    experimental_telemetry: TODO,
-  });
+            Generate a title for the conversation.
+            Return only the title.
+        `,
+        experimental_telemetry: {
+            isEnabled: true,
+            functionId: 'get-titles',
+            metadata: {
+                langfuseTraceId: trace.id,
+            },
+        },
+    });
 
-  const stream = streamTextResult.toUIMessageStream({
-    onFinish: async () => {
-      const title = await titleResult;
+    const streamTextResult = streamText({
+        model: google('gemini-2.0-flash'),
+        messages: modelMessages,
+        // TODO: declare the experimental_telemetry property using the following object:
+        // - isEnabled: true
+        // - functionId: 'your-name-here'
+        // - metadata: { langfuseTraceId: trace.id }
+        experimental_telemetry: {
+            isEnabled: true,
+            functionId: 'stream-test',
+            metadata: {
+                langfuseTraceId: trace.id,
+            },
+        },
+    });
 
-      console.log('title: ', title.text);
+    const stream = streamTextResult.toUIMessageStream({
+        onFinish: async () => {
+            const title = await titleResult;
 
-      // TODO: flush the langfuse traces using the langfuse.flushAsync method
-      // and await the result
-      TODO;
-    },
-  });
+            console.log('title: ', title.text);
 
-  return createUIMessageStreamResponse({
-    stream,
-  });
+            // TODO: flush the langfuse traces using the langfuse.flushAsync method
+            // and await the result
+            await langfuse.flushAsync();
+        },
+    });
+
+    return createUIMessageStreamResponse({
+        stream,
+    });
 };
